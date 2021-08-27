5f3a2be1c73ed25937bd93b54c86f55d4b5d2b8e
Mateusz <mateuszzygadlo00@gmail.com>
2021-08-27 23:40:25 +0200

    Very dirty code

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ const gameArea = document.querySelectorAll('.gameArea');
 const counter = document.querySelector('.counter');
 let count = 1;
 let choice;
-let winArr = [];
 
 const game = () => {
 
@@ -23,16 +22,17 @@ const game = () => {
 
 }
 
-const winnerGame = () => {
+const rowWinner = () => {
 
     const rowOne = gameArr.slice(0, 3);
     const rowTwo = gameArr.slice(3, 6);
     const rowThree = gameArr.slice(6, 9);
-    const columnOne = [gameArr[0], gameArr[3], gameArr[6]];
-    const columnTwo = [gameArr[1], gameArr[4], gameArr[7]];
-    const columnThree = [gameArr[2], gameArr[5], gameArr[8]];
-    const diagonalOne = [gameArr[0], gameArr[4], gameArr[8]];
-    const diagonalTwo = [gameArr[2], gameArr[4], gameArr[6]];
+
+    // const columnOne = [gameArr[0], gameArr[3], gameArr[6]];
+    // const columnTwo = [gameArr[1], gameArr[4], gameArr[7]];
+    // const columnThree = [gameArr[2], gameArr[5], gameArr[8]];
+    // const diagonalOne = [gameArr[0], gameArr[4], gameArr[8]];
+    // const diagonalTwo = [gameArr[2], gameArr[4], gameArr[6]];
 
     return{
         rowOne,
@@ -67,7 +67,21 @@ gameArea.forEach(item => {
         gameArea[e.target.dataset.id].textContent = choice;
         gameArr[e.target.dataset.id] = choice;
 
-        
+        if(!rowWinner().rowOne.some(item => item == '')){
+            if(rowWinner().rowOne.every(item => item == rowWinner().rowOne[0])){
+                console.log(rowWinner().rowOne);
+            }
+        }
+        if(!rowWinner().rowTwo.some(item => item == '')){
+            if(rowWinner().rowTwo.every(item => item == rowWinner().rowTwo[0])){
+                console.log(rowWinner().rowTwo);
+            }
+        }
+        if(!rowWinner().rowThree.some(item => item == '')){
+            if(rowWinner().rowThree.every(item => item == rowWinner().rowThree[0])){
+                console.log(rowWinner().rowThree);
+            }
+        }
 
         count++;
     })
