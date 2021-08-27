43567439ec11c634be7b8ae4358ed1a3fb68ddaa
Mateusz <mateuszzygadlo00@gmail.com>
2021-08-27 22:54:26 +0200

    Edit

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const gameArea = document.querySelectorAll('.gameArea');
 const counter = document.querySelector('.counter');
 let count = 1;
 let choice;
+let winArr = [];
 
 const game = () => {
 
@@ -21,6 +22,25 @@ const game = () => {
     };
 
 }
+
+const winnerGame = () => {
+
+    const rowOne = gameArr.slice(0, 3);
+    const rowTwo = gameArr.slice(3, 6);
+    const rowThree = gameArr.slice(6, 9);
+    const columnOne = [gameArr[0], gameArr[3], gameArr[6]];
+    const columnTwo = [gameArr[1], gameArr[4], gameArr[7]];
+    const columnThree = [gameArr[2], gameArr[5], gameArr[8]];
+    const diagonalOne = [gameArr[0], gameArr[4], gameArr[8]];
+    const diagonalTwo = [gameArr[2], gameArr[4], gameArr[6]];
+
+    return{
+        rowOne,
+        rowTwo,
+        rowThree,
+    }
+}
+
 
 const gameArr = game().gameBoard;
 
@@ -46,8 +66,9 @@ gameArea.forEach(item => {
 
         gameArea[e.target.dataset.id].textContent = choice;
         gameArr[e.target.dataset.id] = choice;
+
         
+
         count++;
-        console.log(gameArr);
     })
 })
